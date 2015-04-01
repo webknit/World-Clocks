96386594b3482e6cf84e183b484f24fd6b8b4ebc
Shane Prendergast <contact@webknit.co.uk>
2015-04-01 11:58:47 +0100

    adding initial files

---
+++ assets/js/dev.js
@@ -0,0 +1,10 @@
+/* - BASE HTML TEMPLATE
+------------------------------------------------- 
+	Description: Dev.js
+	Author:Shane Prendergast
+	Author URL:http://www.webknit.co.uk
+	Template URL:http://base.webknit.co.uk/
+	
+	You can read more abotu this file - http://shaneprendergast.co.uk/web-design/dev-js/
+*/
+
