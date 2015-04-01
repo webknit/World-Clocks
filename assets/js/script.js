bd7a8190ac189f8385ce0f0c2b2bb50663d968a6
Shane Prendergast <contact@webknit.co.uk>
2015-04-01 15:49:53 +0100

    making responsive, adding colour names

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@
 				elements[c].style.transform = 'rotateZ('+ hands[i].angle +'deg)';
 
 				// If this is a minute hand, note the seconds position (to calculate minute position later)
-				if (hands[c].hand === 'minutes') {
+				if (hands[i].hand === 'clock__mins') {
 
 					elements[c].parentNode.setAttribute('data-second-angle', hands[i + 1].angle);
 
@@ -42,6 +42,95 @@
 			}
 
 		}
+
+		moveSecondHands();
+		setUpMinuteHands();
+
+	}
+
+	function setUpMinuteHands() {
+
+		// Find out how far into the minute we are
+		var containers = document.querySelectorAll('.clock__container--mins');
+		var secondAngle = containers[0].getAttribute("data-second-angle");
+
+		if (secondAngle > 0) {
+
+			// Set a timeout until the end of the current minute, to move the hand
+			var delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
+
+			setTimeout(function() { 
+
+				moveMinuteHands(containers);
+
+			}, delay);
+
+		}
+
+	}
+
+	function moveMinuteHands(containers) {
+
+		for (var i = 0; i < containers.length; i++) {
+
+			containers[i].style.webkitTransform = 'rotateZ(6deg)';
+			containers[i].style.transform = 'rotateZ(6deg)';
+
+		}
+		// Then continue with a 60 second interval
+		setInterval(function() {
+
+			for (var i = 0; i < containers.length; i++) {
+
+				if (containers[i].angle === undefined) {
+
+					containers[i].angle = 12;
+
+				}
+
+				else {
+
+					containers[i].angle += 6;
+
+				}
+
+				containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
+				containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
+
+			}
+
+		}, 60000);
+
+	}
+
+	/*
+	* Move the second containers
+	*/
+	function moveSecondHands() {
+
+		var containers = document.querySelectorAll('.clock__container--seconds');
+
+		setInterval(function() {
+
+		for (var i = 0; i < containers.length; i++) {
+
+			if (containers[i].angle === undefined) {
+
+				containers[i].angle = 6;
+
+			} 
+
+			else {
+
+				containers[i].angle += 6;
+			}
+
+			containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
+			containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
+
+			}
+
+		}, 1000);
 
 	}
 
