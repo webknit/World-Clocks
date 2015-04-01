a3fe0031970e356331cf586b403bb3a545d866e9
Shane Prendergast <contact@webknit.co.uk>
2015-04-01 12:33:53 +0100

    Making the clock start at the correct time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,50 @@
-/* - BASE HTML TEMPLATE
-------------------------------------------------- 
-	Description: JS Scripts
-	Author:Shane Prendergast
-	Author URL:http://www.webknit.co.uk
-	Template URL:http://base.webknit.co.uk/
-	
-	If you're not using Grunt, or want to know how to use grunt, then have a read of the following post for more info.
-	http://shaneprendergast.co.uk/css/a-guide-to-base/
-*/
-
-// JS EXAMPLE
-
-var Base = Base || {};
-
-Base.functionName = function() {
-	
-	var variable = $('.variable');
+(function () {
 
 	function init() {
 
-		variable.click(functionOne);
+		var date = new Date();
+		var hours = date.getHours();
+		var mins = date.getMinutes();
+		var seconds = date.getSeconds();
 
-	}
+		var hands = [
+			{
+				hand: 'clock__hours',
+				angle: (hours * 30) + (mins / 2)
+			},
+			{
+				hand: 'clock__mins',
+				angle: (mins * 6)
+			},
+			{
+				hand: 'clock__seconds',
+				angle: (seconds * 6)
+			}
+		];
 
-	function functionOne() {
+		for(i = 0; i < hands.length; i++) {
 
-		// JS CODE
+			var elements = document.querySelectorAll('.' + hands[i].hand);
+
+			for(c = 0; c < elements.length; c++) {
+
+				// For webkit browsers
+				elements[c].style.webkitTransform = 'rotateZ('+ hands[i].angle +'deg)';
+				elements[c].style.transform = 'rotateZ('+ hands[i].angle +'deg)';
+
+				// If this is a minute hand, note the seconds position (to calculate minute position later)
+				if (hands[c].hand === 'minutes') {
+
+					elements[c].parentNode.setAttribute('data-second-angle', hands[i + 1].angle);
+
+				}
+
+			}
+
+		}
 
 	}
 
 	init();
-	
-};
 
-// ON DOC READY
-$(function() {
-	
-	new Base.functionName();
-	
-});
-
+})();
