9ffa2f1fe16a4e56ac8e5a582f084785c14cecd4
Shane Prendergast <contact@webknit.co.uk>
2015-04-01 18:40:37 +0100

    added all the countires in

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,112 @@
 
 	function init() {
 
+		startInternationalClocks();
+		startLocalClock();
+		moveSecondHands();
+		setUpMinuteHands();
+
+	}
+
+	function internationlTimes() {
+
+		// https://raw.githubusercontent.com/moment/moment-timezone/develop/data/packed/latest.json
+
+		moment.tz.add([
+			'Eire|GMT IST|0 -10|01010101010101010101010|1BWp0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00',
+			'Europe/Paris|PMT WET WEST CEST CET WEMT|-9.l 0 -10 -20 -10 -20|0121212121212121212121212121212121212121212121212123434352543434343434343434343434343434343434343434343434343434343434343434343434343434343434343434343434343434343434343434343434343434|-2nco8.l cNb8.l HA0 19A0 1iM0 11c0 1oo0 Wo0 1rc0 QM0 1EM0 UM0 1u00 10o0 1io0 1wo0 Rc0 1a00 1fA0 1cM0 1cM0 1io0 17c0 1fA0 1a00 1io0 1a00 1io0 17c0 1fA0 1a00 1io0 17c0 1cM0 1cM0 1a00 1io0 1cM0 1cM0 1a00 1fA0 1io0 17c0 1cM0 1cM0 1a00 1fA0 1io0 1qM0 Df0 Ik0 5M30 WM0 1fA0 1cM0 Vx0 hB0 1aq0 16M0 1ekn0 1cL0 1fC0 1a00 1fA0 1cM0 1cM0 1cM0 1fA0 1a00 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00',
+			"Asia/Hong_Kong|LMT HKT HKST JST|-7A.G -80 -90 -90|0121312121212121212121212121212121212121212121212121212121212121212121|-2CFHA.G 1sEP6.G 1cL0 ylu 93X0 1qQu 1tX0 Rd0 1In0 NB0 1cL0 11B0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1kL0 14N0 1nX0 U10 1tz0 U10 1wn0 Rd0 1wn0 U10 1tz0 U10 1tz0 U10 1tz0 U10 1wn0 Rd0 1wn0 Rd0 1wn0 U10 1tz0 U10 1tz0 17d0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 s10 1Vz0 1cN0 1cL0 1cN0 1cL0 6fd0 14n0",
+			'America/New_York|EST EDT|50 40|0101|1Lz50 1zb0 Op0',
+			"America/Los_Angeles|PST PDT PWT PPT|80 70 70 70|010102301010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010|-261q0 1nX0 11B0 1nX0 SgN0 8x10 iy0 5Wp0 1Vb0 3dB0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 s10 1Vz0 LB0 1BX0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0"
+		]);
+
+		var now = new Date();
+
+		var times = [
+			{
+			  jsclass: 'london',
+			  jstime: moment.tz(now, "Eire")
+			},
+			{
+			  jsclass: 'new-york',
+			  jstime: moment.tz(now, "America/New_York")
+			},
+			{
+			  jsclass: 'paris',
+			  jstime: moment.tz(now, "Europe/Paris")
+			},
+			{
+			  jsclass: 'shanghai',
+			  jstime: moment.tz(now, "Asia/Hong_Kong")
+			},
+			{
+			  jsclass: 'san-fran',
+			  jstime: moment.tz(now, "America/Los_Angeles")
+			}
+		];
+
+		return times;
+
+	}
+
+	function startInternationalClocks() {
+
+		var times = internationlTimes();
+
+		for (i = 0; i < times.length; i++) {
+
+			var hours = times[i].jstime.format('hh');
+		    var mins = times[i].jstime.format('mm');
+		    var seconds = times[i].jstime.format('s');
+
+		    console.log(hours, mins, seconds);
+
+			var hands = [
+				{
+					hand: 'clock__hours',
+					angle: (hours * 30) + (mins / 2)
+				},
+				{
+					hand: 'clock__mins',
+					angle: (mins * 6)
+				},
+				{
+					hand: 'clock__seconds',
+					angle: (seconds * 6)
+				}
+			];
+
+			for(j = 0; j < hands.length; j++) {
+
+				var elements = document.querySelectorAll('.time-zones__wrapper .' + times[i].jsclass + ' .' + hands[j].hand);
+
+				for(c = 0; c < elements.length; c++) {
+
+					// For webkit browsers
+					elements[c].style.webkitTransform = 'rotateZ('+ hands[j].angle +'deg)';
+					elements[c].style.transform = 'rotateZ('+ hands[j].angle +'deg)';
+
+					// If this is a minute hand, note the seconds position (to calculate minute position later)
+					if (hands[j].hand === 'clock__mins') {
+
+						elements[c].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
+	function startLocalClock() {
+
 		var date = new Date();
-		var hours = date.getHours();
-		var mins = date.getMinutes();
 		var seconds = date.getSeconds();
+		var mins = date.getMinutes();
+		var hours = date.getHours();
 
 		var hands = [
 			{
@@ -24,7 +126,7 @@
 
 		for(i = 0; i < hands.length; i++) {
 
-			var elements = document.querySelectorAll('.' + hands[i].hand);
+			var elements = document.querySelectorAll('.local .' + hands[i].hand);
 
 			for(c = 0; c < elements.length; c++) {
 
@@ -42,16 +144,13 @@
 			}
 
 		}
-
-		moveSecondHands();
-		setUpMinuteHands();
-
+		
 	}
 
 	function setUpMinuteHands() {
 
 		// Find out how far into the minute we are
-		var containers = document.querySelectorAll('.clock__container--mins');
+		var containers = document.querySelectorAll('.clock .clock__container--mins');
 		var secondAngle = containers[0].getAttribute("data-second-angle");
 
 		if (secondAngle > 0) {
